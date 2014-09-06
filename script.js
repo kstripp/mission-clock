72fb11aa87e851ad7a5eaf651e296206f9140699
K. Scott Tripp <ktripp@umich.edu>
2014-09-05 23:05:33 -0700

    Elapsed time works correctly
    UTC is technically correct, but not useful

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var interval = setInterval(function() {update_clocks()}, 100);
+var launch_date = new Date('1970-01-01T00:00:00Z');
 
 function update_clocks()
 {
-	var time = new Date()
+	var time = new Date();
+	time = time - launch_date;
+	time = new Date(time);
+	
+	var days 	= Math.floor(time / (1000 * 60 * 60 * 24));
 	var hours   = format_time_digits(time.getUTCHours());
 	var minutes = format_time_digits(time.getUTCMinutes());
 	var seconds = format_time_digits(time.getUTCSeconds());
-	document.getElementById("clock1").innerHTML = hours + ":" + minutes + ":" + seconds;
+	document.getElementById("clock1").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
 }
 
 function format_time_digits(value)
@@ -34,14 +39,22 @@ function hide_menu()
 	sidebar.style.marginLeft="-300px";
 }
 
-function change_mission(name, launch_date)
+function change_mission(name, date)
 {
 	label = document.getElementById("label1");
+	
+	now = new Date();
+	launch_date = new Date(date);
 	
 	if (name == "UTC")
 	{	
 		label.innerHTML = "UTC";
 	} else {
-		label.innerHTML = name + " Mission Elapsed Time";
+		if (now > launch_date)
+		{
+			label.innerHTML = name + " Mission Elapsed Time";
+		} else {
+			label.innerHTML = name + " Launch";
+		}
 	}
 }
