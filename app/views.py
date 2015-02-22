f985a8dcf67355b72d1945876fd87ab7f6519ac6
K. Scott Tripp <kscott.tripp@gmail.com>
2015-02-21 22:38:02 -0800

    URL "dynamically" added based on missions

--- app/views.py
+++ app/views.py
@@ -3,26 +3,37 @@
 from app import app
 import flask
 
-missions = [
-# stand in array of  missionss
-        {
-            'label':'UTC',
-            'launch_date':'1970-01-01T00:00:00Z',
-            'url':'/utc'
-        },
-        {
-            'label':'M-Cubed',
-            'launch_date':'2011-10-28T19:48:01Z',
-            'url':'/m-cubed'
-        }
-    ]
-    
+class Mission(object):
+
+    def __init__(self, **kwargs):
+        label = kwargs['label']
+        launch_date = kwargs['launch_date']
+        url = kwargs['url']
+        next_event = None
+
+missions = []
+
+missions.append({'label':'UTC',         'launch_date':'1970-01-01T00:00:00Z', 'url':'/utc'})
+missions.append({'label':'M-Cubed',     'launch_date':'2011-10-28T19:48:01Z', 'url':'/m-cubed'})
+missions.append({'label':'MCubed-2',    'launch_date':'2013-12-06T06:30:00Z', 'url':'/mcubed-2'})
+missions.append({'label':'RAX',         'launch_date':'2010-11-20T01:25:00Z', 'url':'/rax'})
+missions.append({'label':'RAX-2',       'launch_date':'2011-10-28T19:48:01Z', 'url':'/rax-2'})
+missions.append({'label':'E1P',         'launch_date':'2011-03-04T10:09:43Z', 'url':'/e1p'})
+missions.append({'label':'E1P-FU2',     'launch_date':'2011-10-28T19:48:01Z', 'url':'/e1p-fu2'})
+missions.append({'label':'GRIFEX',      'launch_date':'2015-01-31T14:22:42Z', 'url':'/grifex'})
+missions.append({'label':'FIREBIRD II', 'launch_date':'2015-01-31T14:22:42Z', 'url':'/firebirdii'})
+
+urls = []
+for i in missions:
+    urls.append(i['url'])
+
 @app.route('/')
 @app.route('/index')
 @app.route('/utc')
 def index():
-    return flask.render_template('index.html', title="Mission Clock", missions=missions, clock_label='UTC',mission=missions[0])
+    return flask.render_template('index.html', title="Mission Clock", missions=missions, clock_label=missions[0]['label'],mission=missions[0])
 
-@app.route('/m-cubed')
-def m_cubed():
-    return flask.render_template('index.html', title="Mission Clock", missions=missions, clock_label='M-Cubed', mission=missions[1])
+@app.route('/<path:path>')
+def m_cubed(path):
+    idx = urls.index('/' + path)
+    return flask.render_template('index.html', title="Mission Clock", missions=missions, clock_label=missions[idx]['label'],mission=missions[idx])
