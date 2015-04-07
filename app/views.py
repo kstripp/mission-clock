d541bc6a393a72254c6d0138fb513f356e268f21
K. Scott Tripp <ktripp@umich.edu>
2015-04-06 21:41:24 -0700

    Fixed URL generator for freezer

--- app/views.py
+++ app/views.py
@@ -13,23 +13,23 @@ class Mission(object):
 
 missions = []
 
-missions.append({'label':'UTC',         'launch_date':'1970-01-01T00:00:00Z', 'url':'/utc'})
-missions.append({'label':'M-Cubed',     'launch_date':'2011-10-28T19:48:01Z', 'url':'/m-cubed'})
-missions.append({'label':'MCubed-2',    'launch_date':'2013-12-06T06:30:00Z', 'url':'/mcubed-2'})
-missions.append({'label':'RAX',         'launch_date':'2010-11-20T01:25:00Z', 'url':'/rax'})
-missions.append({'label':'RAX-2',       'launch_date':'2011-10-28T19:48:01Z', 'url':'/rax-2'})
-missions.append({'label':'E1P',         'launch_date':'2011-03-04T10:09:43Z', 'url':'/e1p'})
-missions.append({'label':'E1P-FU2',     'launch_date':'2011-10-28T19:48:01Z', 'url':'/e1p-fu2'})
-missions.append({'label':'GRIFEX',      'launch_date':'2015-01-31T14:22:42Z', 'url':'/grifex'})
-missions.append({'label':'FIREBIRD II', 'launch_date':'2015-01-31T14:22:42Z', 'url':'/firebirdii'})
+missions.append({'label':'UTC',         'launch_date':'1970-01-01T00:00:00Z', 'url':'/utc.html'})
+missions.append({'label':'M-Cubed',     'launch_date':'2011-10-28T19:48:01Z', 'url':'/m-cubed.html'})
+missions.append({'label':'MCubed-2',    'launch_date':'2013-12-06T06:30:00Z', 'url':'/mcubed-2.html'})
+missions.append({'label':'RAX',         'launch_date':'2010-11-20T01:25:00Z', 'url':'/rax.html'})
+missions.append({'label':'RAX-2',       'launch_date':'2011-10-28T19:48:01Z', 'url':'/rax-2.html'})
+missions.append({'label':'E1P',         'launch_date':'2011-03-04T10:09:43Z', 'url':'/e1p.html'})
+missions.append({'label':'E1P-FU2',     'launch_date':'2011-10-28T19:48:01Z', 'url':'/e1p-fu2.html'})
+missions.append({'label':'GRIFEX',      'launch_date':'2015-01-31T14:22:42Z', 'url':'/grifex.html'})
+missions.append({'label':'FIREBIRD II', 'launch_date':'2015-01-31T14:22:42Z', 'url':'/firebirdii.html'})
 
 urls = []
 for i in missions:
     urls.append(i['url'])
 
 @app.route('/')
-@app.route('/index')
-@app.route('/utc')
+@app.route('/index.html')
+@app.route('/utc.html')
 def index():
     return flask.render_template('index.html', title="Mission Clock", missions=missions, clock_label=missions[0]['label'],mission=missions[0])
 
